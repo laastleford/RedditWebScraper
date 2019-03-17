b2282af6443fd84a0b2f749851100479c555e442
Lindsay <laastleford@gmail.com>
2019-03-17 08:13:19 -0500

    First commit

---
+++ BotWeb/BotWeb/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading;
+
+namespace BotWeb
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            IWebDriver _driver;
+           
+            _driver = new ChromeDriver("C:\\Users\\laast\\.nuget\\packages\\selenium.webdriver.chromedriver\\73.0.3683.68\\driver\\win32");
+            _driver.Navigate().GoToUrl("https://reddit.com");      
+
+            IList<IWebElement> titles = _driver.FindElements(By.XPath("//*[@class='s1okktje-0 kCqBrs']")) ;
+
+            foreach (var title in titles)
+            {
+                Console.WriteLine(title.Text);
+                Thread.Sleep(300);
+            }
+                       
+        }
+    }
+   
+}
